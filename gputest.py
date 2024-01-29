cb5c8751bcb6db4c5f33c692cfbfe071ec90cc2d
anubhav singh <audiq4456@gmail.com>
2024-01-29 19:20:54 +0530

    updated with device nerd stats

--- gputest.py
+++ gputest.py
@@ -1,4 +1,9 @@
 # Desc: Test GPU availability
 import torch
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0),"Haan bhai gpu hai tere paas")
+print("Haan cuda available hai tere system mein",torch.cuda.is_available())
+print(torch.cuda.get_device_name(0)," gpu hai tere paas")
+print(torch.cuda.device_count())
+print(torch.cuda.current_device())
+print(torch.cuda.memory_allocated())
+print(torch.cuda.memory_reserved())
+print(torch.cuda.get_device_capability())
